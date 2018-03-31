423e7517f0af11bb926f522814d4d76da54b7055
Clifford Paulick <tko@tourkick.com>
2018-03-31 14:53:17 -0500

    Add `GitHub Plugin URI` to plugin header

--- extension-template.php
+++ extension-template.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name:     [Base Plugin Name] Extension: [Extension Name]
- * Description:     [Extension Description]
- * Version:         1.0.0
- * Extension Class: Tribe__Extension__Example
- * Author:          Modern Tribe, Inc.
- * Author URI:      http://m.tri.be/1971
- * License:         GPL version 3 or any later version
- * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:     match-the-plugin-directory-name
+ * Plugin Name:       [Base Plugin Name] Extension: [Extension Name]
+ * Description:       [Extension Description]
+ * Version:           1.0.0
+ * Extension Class:   Tribe__Extension__Example
+ * GitHub Plugin URI: https://github.com/mt-support/match-the-plugin-directory-name
+ * Author:            Modern Tribe, Inc.
+ * Author URI:        http://m.tri.be/1971
+ * License:           GPL version 3 or any later version
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       match-the-plugin-directory-name
  *
  *     This plugin is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
