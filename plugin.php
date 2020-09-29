33ac0572b03fc178f90ca96b4cc35ef92a963a5e
Crisoforo Gaspar <hello@crisoforo.com>
2020-09-29 15:54:45 -0500

    Sintax error

--- plugin.php
+++ plugin.php
@@ -55,7 +55,8 @@ function tribe_extension___TRIBE_SLUG_CLEAN__() {
 			'__TRIBE_SLUG__',
 			'<p>' . esc_html__( 'Couldn\'t properly load "__TRIBE_BASE__ Extension: __TRIBE_NAME__" the extension was deactivated.', '__TRIBE_DOMAIN__' ) . '</p>',
 			[],
-			1, // 1 second after that make sure the transiet is removed.
+			// 1 second after that make sure the transiet is removed.
+			1 
 		);
 
 		if ( ! function_exists( 'deactivate_plugins' ) ) {
